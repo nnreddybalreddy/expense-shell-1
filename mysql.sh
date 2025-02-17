6e19579ed7cef0ae5c10a0e1ca630e45a60a45c3
nnreddybalreddy <narendra.mahn@gmail.com>
2025-02-16 20:08:42 -0500

    ALL

---
+++ mysql.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+TIMESTAMP=$(date +%F-%M-%H-%S)
+LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
+
+USERID=$(id -u)
+
+if [ $USERID -ne 0 ]
+then 
+    echo "Be a root user"
+    exit 1
+else 
+    echo "You are super"    
+fi 
+
+VALIDATE(){
+    if [ $? -eq 0 ]
+    then 
+        echo -e "$2 $G success $N"
+    else 
+        echo -e "$2 $R failure $N"    
+    fi 
+}
+
+
+dnf list installed mysql-server &>>$LOGFILE
+
+if [ $? -eq 0 ]
+then 
+    echo "mysql-server already installed"
+else 
+    dnf install mysql-server -y &>>$LOGFILE 
+fi 
+
