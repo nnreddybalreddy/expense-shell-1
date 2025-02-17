18f09b1133d064547813a1433590bad15f6ad253
nnreddybalreddy <narendra.mahn@gmail.com>
2025-02-17 07:39:30 -0500

    ALL

--- frontend.sh
+++ frontend.sh
@@ -1,35 +1,5 @@
 #!/bin/bash
 
-R="\e[31m"
-G="\e[32m"
-Y="\e[33m"
-N="\e[0m"
-
-SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
-TIMESTAMP=$(date +%F-%M-%H-%S)
-LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
-
-USERID=$(id -u)
-
-# echo "Enter db password:::"
-# read mysql_root_password
-
-if [ $USERID -ne 0 ]
-then 
-    echo "Be a root user"
-    exit 1
-else 
-    echo "You are super"    
-fi 
-
-VALIDATE(){
-    if [ $? -eq 0 ]
-    then 
-        echo -e "$2 $G success $N"
-    else 
-        echo -e "$2 $R failure $N"    
-    fi 
-}
 
 
 dnf install nginx -y &>>$LOGFILE 
