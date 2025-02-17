6c2e8409d8ea389861eac8f6fbf54467b7ba9625
nnreddybalreddy <narendra.mahn@gmail.com>
2025-02-16 20:49:06 -0500

    ALL

--- backend.sh
+++ backend.sh
@@ -71,6 +71,25 @@ VALIDATE $? "npm install"
 cp -rf /home/ec2-user/expense-shell-1/backend.service /etc/systemd/system/backend.service &>>$LOGFILE 
 VALIDATE $? "copied service"
 
+systemctl daemon-reload &>>$LOGFILE 
+VALIDATE $? "daemon reload" 
+
+systemctl start backend &>>$LOGFILE 
+VALIDATE $? "start backend"
+
+systemctl enable backend  &>>$LOGFILE 
+VALIDATE $? "enable backend"
+
+dnf install mysql -y >>$LOGFILE 
+VALIDATE $? "mysql install"
+
+mysql -h db.narenda.shop -uroot -p${mysql_root_password} < /app/schema/backend.sql >>$LOGFILE 
+VALIDATE $? "scheam load"
+
+systemctl restart backend >>$LOGFILE 
+VALIDATE $? "backend restart"
+
+
 
 
 
