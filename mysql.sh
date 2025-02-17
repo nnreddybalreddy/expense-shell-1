8bcee6cf6cb8e11fe28d77cb708f06ee3fa4499e
nnreddybalreddy <narendra.mahn@gmail.com>
2025-02-17 07:32:16 -0500

    ALL

--- mysql.sh
+++ mysql.sh
@@ -1,35 +1,11 @@
 #!/bin/bash
 
-R="\e[31m"
-G="\e[32m"
-Y="\e[33m"
-N="\e[0m"
-
-SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
-TIMESTAMP=$(date +%F-%M-%H-%S)
-LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
-
-USERID=$(id -u)
+source ./common.sh
 
 echo "Enter db password:::"
 read mysql_root_password
 
-if [ $USERID -ne 0 ]
-then 
-    echo "Be a root user"
-    exit 1
-else 
-    echo "You are super"    
-fi 
-
-VALIDATE(){
-    if [ $? -eq 0 ]
-    then 
-        echo -e "$2 $G success $N"
-    else 
-        echo -e "$2 $R failure $N"    
-    fi 
-}
+check_root()
 
 
 dnf install mysql-server -y &>>$LOGFILE 
